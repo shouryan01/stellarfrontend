ddfa896733f055aeec00b73a056250b8fa93f680
adityakhambete <76214901+adityakhambete@users.noreply.github.com>
2022-04-24 03:17:18 -0400

    Add files via upload

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -3,10 +3,10 @@ import Placeholder from "../Utils/Placeholder";
 
 export default function Summary (): JSX.Element {
     return (
+    <div> 
+        <iframe src="https://maps.co/embed/6264b9e3084dd531178644ujk1a3f78" width="800" height="600" frameborder="0" allowfullscreen></iframe>
         <div>
-            Summary Page
-            <Placeholder />
+            <a href="https://maps.co">Powered by MAPS</a>
         </div>
-        
-    )
+    </div>)
 };
